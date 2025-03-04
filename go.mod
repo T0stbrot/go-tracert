7df12135fd018b9b47fc47a1458e7cff27cc0c7e
T0stbrot <51487454+T0stbrot@users.noreply.github.com>
2025-03-04 15:34:58 +0100

    Add files via upload
    Initial Uploads

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/t0stbrot/go-tracert
+
+go 1.24.0
+
+require (
+	github.com/t0stbrot/go-ping v0.0.4 // indirect
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	ping "github.com/t0stbrot/go-ping"
+)
+
+type HopEntry struct {
+	Address string `json:"address,omitempty"`
+	RTT     string `json:"rtt,omitempty"`
+	Error   string `json:"error,omitempty"`
+}
+
+type TracerouteResult struct {
+	Target  string     `json:"target"`
+	MaxHops int        `json:"maxhops"`
+	Hops    []HopEntry `json:"hops"`
+	Message string     `json:"message,omitempty"`
+	Error   string     `json:"error,omitempty"`
+}
+
+func Traceroute4(target string, maxhops int, timeout int) (result TracerouteResult) {
+	ttl := 1
+	res := TracerouteResult{Target: target, MaxHops: maxhops}
+	for ttl <= maxhops {
+		cur := ping.Ping4(target, ttl, timeout)
+		ttl++
+		if cur.Message == "suceed" {
+			resEntry := HopEntry{Address: cur.LastHop, RTT: cur.RTT}
+			res.Hops = append(res.Hops, resEntry)
+			break
+		} else if cur.Message == "timeexceed" {
+			resEntry := HopEntry{Address: cur.LastHop, RTT: cur.RTT}
+			res.Hops = append(res.Hops, resEntry)
+		} else if cur.Error != "" {
+			resEntry := HopEntry{Address: cur.LastHop, RTT: cur.RTT, Error: cur.Error}
+			res.Hops = append(res.Hops, resEntry)
+		}
+	}
+	return res
+}
