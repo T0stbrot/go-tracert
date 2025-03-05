cf1bf38b1a9df313b77c8906a62fc142c74eec85
T0stbrot <51487454+T0stbrot@users.noreply.github.com>
2025-03-05 14:04:03 +0100

    Update main.go
    Added Traceroute6

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package gotracert
 
 import (
-	ping "github.com/t0stbrot/go-ping"
+	ping "github.com/t0stbrot/go-ping@v0.0.5"
 )
 
 type HopEntry struct {
@@ -24,10 +24,37 @@ func Traceroute4(target string, maxhops int, timeout int) (result TracerouteResu
 	for ttl <= maxhops {
 		cur := ping.Ping4(target, ttl, timeout)
 		ttl++
-		if cur.Message == "suceed" {
+		if cur.Message == "suceed" && cur.LastHop == target {
 			resEntry := HopEntry{Address: cur.LastHop, RTT: cur.RTT}
 			res.Hops = append(res.Hops, resEntry)
 			break
+		} else if cur.Message == "timeexceed" {
+			resEntry := HopEntry{Address: cur.LastHop, RTT: cur.RTT}
+			res.Hops = append(res.Hops, resEntry)
+		} else if cur.Error != "" {
+			resEntry := HopEntry{Address: cur.LastHop, RTT: cur.RTT, Error: cur.Error}
+			res.Hops = append(res.Hops, resEntry)
+		} else {
+			resEntry := HopEntry{Address: cur.LastHop, RTT: cur.RTT, Error: cur.Error}
+			res.Hops = append(res.Hops, resEntry)
+		}
+	}
+	return res
+}
+
+func Traceroute6(target string, maxhops int, timeout int) (result TracerouteResult) {
+	ttl := 1
+	res := TracerouteResult{Target: target, MaxHops: maxhops}
+	for ttl <= maxhops {
+		cur := ping.Ping6(target, ttl, timeout)
+		ttl++
+		if cur.LastHop == target {
+			resEntry := HopEntry{Address: cur.LastHop, RTT: cur.RTT}
+			res.Hops = append(res.Hops, resEntry)
+			break
+		} else if cur.Message == "suceed" {
+			resEntry := HopEntry{Address: cur.LastHop, RTT: cur.RTT}
+			res.Hops = append(res.Hops, resEntry)
 		} else if cur.Message == "timeexceed" {
 			resEntry := HopEntry{Address: cur.LastHop, RTT: cur.RTT}
 			res.Hops = append(res.Hops, resEntry)
